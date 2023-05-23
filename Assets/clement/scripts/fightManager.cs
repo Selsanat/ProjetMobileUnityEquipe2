e389f2fec26e8a15208d3e3e1e7bd014fc84b0cb
clément bostyn <clemclem20002@gmail.com>
2023-05-23 14:14:45 +0200

    entity et fight manager commencé

---
+++ Assets/clement/scripts/fightManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fightManager : MonoBehaviour
+{
+    private bool playerTurn1;
+    private bool playerTurn2;
+    private bool playerTurn3;
+    private bool playerTurn4;
+    private bool ennemiTurn;
+
+
+
+
+}
