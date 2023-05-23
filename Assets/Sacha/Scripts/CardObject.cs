96b1ade8365f57c167516ca272e908fcace07d08
Soul <S.soulSlasher@gmail.com>
2023-05-23 16:38:58 +0200

    [Mod] CardObject

--- Assets/Sacha/Scripts/CardObject.cs
+++ Assets/Sacha/Scripts/CardObject.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NaughtyAttributes;
 
 [ExecuteInEditMode]
 public class CardObject : MonoBehaviour
@@ -15,7 +16,7 @@ public class CardObject : MonoBehaviour
         return rer.sprite;
     }
 
-    [Button]
+    [SerializeField][Button]
     private void OnValidate()
     {
         SetSprite();
