89a5d309861e8681f44d66d41152c1d14c7cb660
Soul <S.soulSlasher@gmail.com>
2023-05-24 11:20:15 +0200

    [mod] Card

--- Assets/Sacha/Scripts/CardObject.cs
+++ Assets/Sacha/Scripts/CardObject.cs
@@ -6,9 +6,23 @@ using NaughtyAttributes;
 [ExecuteInEditMode]
 public class CardObject : MonoBehaviour
 {
+    [Header("LA CARTE EN SCRIPTABLE OBJECT")]
     [SerializeField]private card ScriptableCard;
 
 
+    #region Leandro
+    [Header("             Statistics")]
+    [SerializeField] float RatioGrowHoverCard;
+    void OnMouseOver()
+    {
+        transform.localScale = new Vector3(RatioGrowHoverCard, RatioGrowHoverCard, RatioGrowHoverCard);
+    }
+
+    void OnMouseExit()
+    {
+        transform.localScale = new Vector3(1, 1, 1);
+    }
+    #endregion
     Sprite SetSprite()
     {
         SpriteRenderer rer = GetComponent<SpriteRenderer>();
