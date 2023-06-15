558f2b3e8035a70a2f86657f27ed8e7f0ab7d426
RomsanSanlag <santinoromero77400@gmail.com>
2023-06-15 16:12:09 +0200

    reparation du pretre de l'ui

--- Assets/Santino/Scripts/SearchObjectByGUID.cs
+++ Assets/Santino/Scripts/SearchObjectByGUID.cs
@@ -8,26 +8,11 @@ using UnityEditor;
 
 public class SearchObjectByGUID : MonoBehaviour
 {
-
-    public List<Object> obj;
-
     public string guid;
 
     [Button]
     void search()
     {
         Debug.Log(AssetDatabase.GUIDToAssetPath(guid));
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
